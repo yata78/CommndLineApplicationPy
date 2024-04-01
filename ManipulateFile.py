ce6ba7122732502ed39255bdb195b11059cc12e1
yata78 <tatsuya20000923@icloud.com>
2024-04-01 21:53:34 +0900

    duplicate and replace

--- ManipulateFile.py
+++ ManipulateFile.py
@@ -5,11 +5,14 @@ args = sys.argv
 
 if args[1] == "copy":
 
-    #TODO try文の重複をwith句で定義
+    #TODO try文の重複をwith句で定義 
+    #TODO バリデータ
+    #TODO 機能的には出来ているがめちゃめちゃ汚い
     try:
         inputPath = args[2]
         ifile = open(inputPath)
         iStr = ifile.read()
+        ifile = close()
     except:
         print('コピー元が入力されていない。もしくは存在しません')
 
@@ -28,6 +31,7 @@ if args[1] == "reverse":
         ifile = open(inputPath)
         iStr = ifile.read()
         print(iStr[0])
+        ifile.close()
     except:
         print('コピー元が入力されていない。もしくは存在しません')
     
@@ -41,5 +45,46 @@ if args[1] == "reverse":
     except:
         print('コピー先が入力されていません。')
 
+if args[1] == "duplicate-contents":
+
+    inputPath = args[2]
+    ifile = open(inputPath)
+    iStr = ifile.read()
+    ifile.close()
     
+    ifile2 = open(inputPath,'a')
+
+    try:
+        inputNumber = int(args[3])
+    except:
+        print('複製回数が入力されていません')
+    
+    for i in range(inputNumber):
+        ifile2.write(f'{iStr}')
+    
+    ifile2.close()
+
+if args[1] == "replace-string":
+
+    inputPath = args[2]
+    ifile = open(inputPath)
+    iStr = ifile.read()
+    ifile.close()
+
+    replaceString = args[3]
+    replaceToString = args[4]
+
+    newIStr = iStr.replace(replaceString, replaceString)
+
+    ifile2 = open(inputPath,'w')
+
+    ifile2.write(newIStr)
+
+    ifile2.close()
+
+
+
+
+    
+
     
